ff6f71ac9596293835544d92f169353acd3494a6
medlouay <mohamedlouay.benbelgacem@esprit.tn>
2022-12-14 23:05:38 +0100

    creating controllers and yml config -Mysql ✔✔

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -1,13 +1,27 @@
 package io.getarrays.server;
 
+import io.getarrays.server.enumeration.Status;
+import io.getarrays.server.model.Server;
+import io.getarrays.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static io.getarrays.server.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+@Bean
+    CommandLineRunner run(ServerRepo serverRepo) {
+        return args -> {
+            serverRepo.save(new Server(null,"192.168.1.160","Ubuntu Linux", "16 GB","Personal PC ",
+                    "http://localhost:8080/server/image/sever2.png", SERVER_UP));
+        };
     }
 
 }
